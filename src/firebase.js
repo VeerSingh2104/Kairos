802c51fd742121741849cf1c0effa04fecca550b
VeerSingh2104 <135021132+VeerSingh2104@users.noreply.github.com>
2025-04-10 16:52:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGkHZEIKNmh1LAXu1D6kn02eUqSjhIE7M",
-  authDomain: "kairos-cc7cd.firebaseapp.com",
-  projectId: "kairos-cc7cd",
-  storageBucket: "kairos-cc7cd.firebasestorage.app",
-  messagingSenderId: "396286230303",
-  appId: "1:396286230303:web:472ca65acd99087ab47a2a",
-  measurementId: "G-0CL6WV9YSY"
+  //Ask me personally
 };
 
 // Initialize Firebase
