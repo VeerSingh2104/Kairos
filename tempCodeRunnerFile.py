13c8a458e94a71dd423168a333a6fcbcad47839c
Tanmay Butta <tanmaybutta47@gmail.com>
2025-04-23 11:16:27 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,31 @@
+# Developed by dnoobnerd [https://dnoobnerd.netlify.app]    Made with Streamlit
+
+
+###### Packages Used ######
+import streamlit as st # core package used in this project
+import pandas as pd
+import base64, random
+import time,datetime
+import pymysql
+import os
+import socket
+import platform
+import geocoder
+import secrets
+import io,random
+import plotly.express as px # to create visualisations at the admin session
+import plotly.graph_objects as go
+from geopy.geocoders import Nominatim
+# libraries used to parse the pdf files
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+from streamlit_tags import st_tags
+from PIL import Image
+# pre stored data for prediction purposes
+from Courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
+import nltk
+nltk.download('stopwords')
